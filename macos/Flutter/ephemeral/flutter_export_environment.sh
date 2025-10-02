2d2560d80ef7bc3d64ec3b3f0fa8a32c69334c3e
malak-0 <malak00fm@gmail.com>
2025-10-02 18:03:11 +0300

    Recreated Flutter project structure

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\malak\fvm\versions\3.35.5"
+export "FLUTTER_ROOT=C:\src\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\malak\audoria"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
